590c263bf37847c9d66c79b7b9ed78a175ecfa9a
fajri-farid <fajrijry14@gmail.com>
2024-01-29 22:01:07 +0800

    heat-add checklist

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,14 +1,17 @@
+// ToDoList.jsx
 import React from "react";
 import { Todo } from "./Todo";
 
 export const ToDoList = ({ data }) => {
+  // nge sort list yang done kebawah 
+  const sortedData = data.sort((a, b) => (a.isdone === "true" ? 1 : -1));
+
   return (
-    <main className=" p-4">
-      <h1 className="p-2 text-center font-bold text-xl">Todos!</h1>
-      <div className="space-y-4">
-      {data.map((item) => {
+    <main className="space-y-2 p-2">
+      {sortedData.map((item) => {
         return <Todo key={item._id} item={item} />;
-      })}</div>
+      })}
     </main>
   );
 };
+
