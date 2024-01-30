7af1bdaee819b92cb6b3d52477ba10d8cd9f69ae
fajri-farid <fajrijry14@gmail.com>
2024-01-30 11:28:01 +0800

    commit

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -11,17 +11,12 @@ export const ToDoList = ({ data }) => {
   const sortedData = data.sort((a, b) => (a.isdone === "true" ? 1 : -1));
   const categories = ["health", "daily", "work", "school"];
 
-  // Gunakan useEffect untuk menyortir ulang daftar setiap kali data berubah
   useEffect(() => {
-    // Fungsi untuk mengurutkan todos berdasarkan createdAt (anda bisa sesuaikan)
     const sortedTodos = [...data].sort((a, b) => {
-      // Urutkan secara descending berdasarkan createdAt (anda bisa sesuaikan)
       return new Date(b.createdAt) - new Date(a.createdAt);
     });
-
-    // Update state dengan daftar yang sudah diurutkan
     setTodos(sortedTodos);
-  }, [data]); // Gunakan [data] sebagai dependensi useEffect
+  }, [data]);
 
   return (
     <main className="space-y-2 p-2">
